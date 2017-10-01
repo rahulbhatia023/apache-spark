a93d869b971d071c16af958ae4ef4c5da95cabf9
Rahul Bhatia <rahul.bhatia023@gmail.com>
2017-10-01 17:37:19 +0530

    .

--- spark-context/src/main/scala/com/rahul/spark/sparkcontext/WordCountExample.scala
+++ spark-context/src/main/scala/com/rahul/spark/sparkcontext/WordCountExample.scala
@@ -3,9 +3,9 @@ package com.rahul.spark.sparkcontext
 import org.apache.spark.SparkContext
 
 object WordCountExample extends App {
-  val sparkContext: SparkContext = InitializeSpark.getSparkContext("employee", "local")
+  val sc = InitializeSpark.getSparkContext("employee", "local")
 
-  val wordsFileRDD = sparkContext.textFile("/home/rahul/Softwares/hadoop-2.8.0/data/words_input.txt")
+  val wordsFileRDD = sc.textFile("/home/rahul/Softwares/hadoop-2.8.0/data/words_input.txt")
   /*
   Array[String] = Array(
   Bus,Car,bus,car,train,car,bus,car,train,bus,TRAIN,BUS,buS,caR,CAR,car,BUS,TRAIN)
@@ -22,29 +22,20 @@ object WordCountExample extends App {
   TRAIN)
   */
 
-  val b = a.map(w => (w, 1))
+  val b = a.countByValue()
   /*
-  Array[(String, Int)] = Array(
-  (Bus,1),
-  (Car,1),
-  (bus,1),
-  (car,1),
-  .....
-  (TRAIN,1))
-   */
+  scala.collection.Map[String,Long] = Map(
+  bus -> 3,
+  TRAIN -> 2,
+  buS -> 1,
+  CAR -> 1,
+  BUS -> 2,
+  car -> 4,
+  caR -> 1,
+  train -> 2,
+  Bus -> 1,
+  Car -> 1)
+  */
 
-  val c = b.reduceByKey((x, y) => x + y)
-  /*
-  Array[(String, Int)] = Array(
-  (TRAIN,2),
-  (Bus,1),
-  (train,2),
-  (Car,1),
-  (buS,1),
-  (car,4),
-  (bus,3),
-  (BUS,2),
-  (CAR,1),
-  (caR,1))
-   */
+  println(b.mkString("\n"))
 }
