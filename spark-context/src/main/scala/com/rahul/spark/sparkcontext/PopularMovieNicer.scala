4d828e7a4f555abc0873c9bce373bdfa38658280
Rahul Bhatia <rahul.bhatia023@gmail.com>
2017-10-02 02:24:46 +0530

    .

---
+++ spark-context/src/main/scala/com/rahul/spark/sparkcontext/PopularMovieNicer.scala
@@ -0,0 +1,30 @@
+package com.rahul.spark.sparkcontext
+
+import scala.io.Source
+
+object PopularMovieNicer extends App {
+  def loadMovieNames(): Map[Int, String] = {
+    var movieNames: Map[Int, String] = Map()
+    val lines = Source.fromFile("/home/rahul/Softwares/hadoop-2.8.0/data/ml-20m/movies.csv").getLines()
+    lines.foreach(line => {
+      val fields = line.split(",")
+      if (fields.length > 0) {
+        movieNames += (fields(0).toInt -> fields(1))
+      }
+    })
+    movieNames
+  }
+
+  val sc = InitializeSpark.getSparkContext("PopularMovie", "local")
+
+  val movieNames = sc.broadcast(loadMovieNames())
+  val a = sc.textFile("/home/rahul/Softwares/hadoop-2.8.0/data/ml-20m/ratings.csv")
+  val b = a.map(x => x.split(",")(1))
+  val c = b.map(x => (x, 1))
+  val d = c.reduceByKey((x, y) => x + y)
+  val e = d.map(x => (x._2, x._1))
+  val f = e.sortByKey()
+  val g = f.map(x => (movieNames.value(x._2.toInt), x._1))
+
+  g.collect().foreach(println)
+}
