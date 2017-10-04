5512a588164af439a90caa909d9b892975d1a1e0
Rahul Bhatia <rahul.bhatia023@gmail.com>
2017-10-05 01:17:53 +0530

    .

---
+++ spark-context/src/main/scala/com/rahul/spark/sparkcontext/MostPopularSuperHero.scala
@@ -0,0 +1,30 @@
+package com.rahul.spark.sparkcontext
+
+object MostPopularSuperHero extends App {
+  val sc = InitializeSpark.getSparkContext("MostPopularSuperHero", "local")
+
+  val names = sc.textFile("/home/rahul/Softwares/hadoop-2.8.0/data/Marvel-names.txt")
+  val namesRDD = names.flatMap(line => parseNames(line))
+  val lines = sc.textFile("/home/rahul/Softwares/hadoop-2.8.0/data/Marvel-graph.txt")
+  val pairing = lines.map(line => countCoOccurences(line))
+  val totalFriendsByCharacter = pairing.reduceByKey((x, y) => x + y)
+  val flipped = totalFriendsByCharacter.map(x => (x._2, x._1))
+  val mostPopular = flipped.max()
+  val mostPopularName = namesRDD.lookup(mostPopular._2)(0)
+
+  println(s"$mostPopularName is the most popular hero with ${mostPopular._1} co-appearances")
+
+  def parseNames(line: String): Option[(Int, String)] = {
+    val fields = line.split("\"")
+    if (fields.length > 1) {
+      return Some(fields(0).trim.toInt, fields(1))
+    } else {
+      return None
+    }
+  }
+
+  def countCoOccurences(line: String) = {
+    val elements = line.split("\\s+")
+    (elements(0).toInt, elements.length - 1)
+  }
+}
