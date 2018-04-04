39c755d4e4bc6dd1ec8481fb1e7e3c44637ca9b6
Rahul Bhatia <rahul.bhatia023@gmail.com>
2018-04-04 23:25:20 +0530

    .

--- spark-context/src/main/scala/com/rahul/spark/sparkcontext/PracticeExercises.scala
+++ spark-context/src/main/scala/com/rahul/spark/sparkcontext/PracticeExercises.scala
@@ -1,5 +1,7 @@
 package com.rahul.spark.sparkcontext
 
+import scala.io.Source
+
 object PracticeExercises extends App {
   val sc = InitializeSpark.getSparkContext("PracticeExercises", "local")
 
@@ -32,7 +34,38 @@ object PracticeExercises extends App {
   // Question - 2b
   val u = users.map(x => x.split(",")).map(x => (x(0), x))
   val t = tweets.map(x => (getTweetParameters(x)._3, x))
+  val usersDictionary = loadUserNames()
 
   val join = u.join(t)
   join.foreach(println)
+
+  // Question - 3a
+  val c = t.countByKey()
+  val d = c.map(x => (usersDictionary(x._1), x._2))
+  d.foreach(println)
+
+  // Question - 3b
+  val e = d.toSeq.sortWith(_._2 > _._2)
+  e.foreach(println)
+
+  def loadUserNames() = {
+    var userNames: Map[String, String] = Map()
+    val lines = Source.fromFile("/home/rahul/Softwares/hadoop-2.8.0/data/users.csv").getLines()
+    lines.foreach(line => {
+      val fields = line.split(",")
+      if (fields.length > 1) {
+        userNames += (fields(0) -> fields(1))
+      }
+    })
+    userNames
+  }
+
+  // Question - 4a
+  val f = d.filter(_._2 >= 2)
+  f.foreach(println)
+
+  // Question - 4b
+  val g = u.leftOuterJoin(t)
+  val h = g.filter(x => x._2._2 == None)
+  h.foreach(x => println(x._1))
 }
